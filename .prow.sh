0f29201561bc6f0410d7b634f10c8e1219f65d55
Opvolger <opvolger@gmail.com>
2026-03-03 16:34:39 +0100

    add risc-v support

--- .prow.sh
+++ .prow.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# Override CSI_PROW_BUILD_PLATFORMS so we can provide riscv64 support
+CSI_PROW_BUILD_PLATFORMS="linux amd64 amd64; linux ppc64le ppc64le -ppc64le; linux s390x s390x -s390x; linux arm arm -arm; linux arm64 arm64 -arm64; linux arm arm/v7 -armv7; linux riscv64 riscv64 -riscv64; windows amd64 amd64 .exe nanoserver:1809 servercore:ltsc2019; windows amd64 amd64 .exe nanoserver:ltsc2022 servercore:ltsc2022"
+
 . release-tools/prow.sh
 
 main
